12b86e1163543b90a9b5a5a258f71208d09f53a0
Cruzer-S <yyyynoom@gmail.com>
2023-07-07 02:28:38 +0900

    Chapter 08: run-make.sh

---
+++ Chapter08/run-make.sh
@@ -0,0 +1,34 @@
+#/bin/bash
+# Assume we are in the source directory
+cur=$PWD
+export SOURCE_DIR=$cur
+
+while [[ $SOURCE_DIR ]]
+do
+	MAKEFILE=$(echo $SOURCE_DIR/[Mm]akefile)
+	if  [[ -e $MAKEFILE ]]
+	then
+		break
+	fi
+
+	SOURCE_DIR=${SOURCE_DIR%/*}
+done
+
+# Print an error if we haven't found a makefile.
+if [[ ! $MAKEFILE ]]
+then
+	printf "$0: Cannot find a makefile\n" > /dev/stderr
+	exit 1
+fi
+
+# Set the output directory to a default, if not set.
+if [[ ! $BINARY_DIR ]]
+then
+	BINARY_DIR=${SOURCE_DIR}/build
+fi
+
+# Create the output directory
+mkdir --parents $BINARY_DIR
+
+# Run the make.
+make --directory="$BINARY_DIR" --file="$MAKEFILE" "$@"
